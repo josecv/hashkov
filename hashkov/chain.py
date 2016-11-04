4e260efbf38a2f221bba1654d0fd8c8a70bea2bc
J <jose.cortesvarela@mail.utoronto.ca>
2016-11-04 18:33:15 +0000

    Implement a basic Markov Chain.

---
+++ hashkov/chain.py
@@ -0,0 +1,42 @@
+'''
+Provides the Markov Chain implementation.
+'''
+import random
+
+
+class MarkovChain(object):
+    '''
+    A Markov chain.
+    '''
+    def __init__(self):
+        '''
+        Construct the Markov chain.
+        '''
+        self.memory = {}
+
+    def train(self, samples):
+        '''
+        Train the markov chain with a list of samples.
+        Each sample itself is a list of tokens that the chain
+        will look at.
+        '''
+        for sample in samples:
+            prev = ''  # The start
+            for token in sample:
+                prev_mem = self.memory.setdefault(prev, [])
+                prev_mem.append(token)
+                prev = token
+
+    def sample(self, length):
+        '''
+        Sample the chain, returning a list of tokens of the length given.
+        '''
+        def inner_sample(length, token):
+            if not length:
+                return [token]
+            token_mem = self.memory.get(token)
+            if token_mem is None or len(token_mem) == 0:
+                return [token]  # Chain's over folks
+            next_token = random.choice(token_mem)
+            return [token] + inner_sample(length - 1, next_token)
+        return inner_sample(length, '')
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Hashkov',
+      version='0.0',
+      description='Tweet generator',
+      author='Jose Cortes-Varela',
+      url='https://www.github.com/josecv/hashkov',
+      packages=['hashkov'],
+      scripts=['scripts/hashkov_tweet'],
+      )
